2270834ce2a9d41b4a14c9b304771e1679396107
Felipe Melo <narulee@hotmail.com>
2020-09-03 20:18:05 -0300

    chat rooms

--- public/js/chat.js
+++ public/js/chat.js
@@ -11,6 +11,9 @@ const $messages = document.querySelector('#messages')
 const messageTemplate = document.querySelector('#message-template').innerHTML
 const locationMessageTemplate = document.querySelector('#location-message-template').innerHTML
 
+// Options
+const { username, room } = Qs.parse(location.search, { ignoreQueryPrefix: true })
+
 socket.on('message', (message) => {
   console.log(message)
   const html = Mustache.render(messageTemplate, {
@@ -67,4 +70,11 @@ $sendLocationButton.addEventListener('click', () => {
       console.log('Location shared!')
     })
   })
+})
+
+socket.emit('join', { username, room }, (error) => {
+  if (error) {
+    alert(error)
+    location.href = '/'
+  }
 })
--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ const express = require('express')
 const socketio = require('socket.io')
 const Filter = require('bad-words')
 const { generateMessage, generateLocationMessage } = require('./utils/messages')
+const { addUser, removeUser } = require('./utils/users')
 
 const app = express()
 const server = http.createServer(app)
@@ -16,9 +17,21 @@ app.use(express.static(publicDirectoryPath))
 
 io.on('connection', (socket) => {
   
-  socket.emit('message', generateMessage('Welcome!'))
-  socket.broadcast.emit('message', generateMessage('New user has joined!'))
-  
+  socket.on('join', ({ username, room }, callback) => {
+    const { error, user } = addUser({id: socket.id, username, room})
+
+    if (error) {
+      return callback(error)
+    }
+
+    socket.join(user.room)
+
+    socket.emit('message', generateMessage('Welcome!'))
+    socket.broadcast.to(user.room).emit('message', generateMessage(`${user.username} has joined!`))
+    
+    callback()
+  })
+
   socket.on('sendMessage', (message, callback) => {
     const filter = new Filter()
 
@@ -36,7 +49,11 @@ io.on('connection', (socket) => {
   })
 
   socket.on('disconnect', () => {
-    io.emit('message', generateMessage('A user has left!'))
+    const user = removeUser(socket.id)
+
+    if (user) {
+      io.to(user.room).emit('message', generateMessage(`${user.username} has left!`))
+    }
   })
 
 })
